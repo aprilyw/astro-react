97c8b65f0b9c05c9978dd906c3c7713be05d027d
April <aprilyanli38@gmail.com>
2021-12-29 16:57:22 -0600

    added react three fiber canvas

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {Canvas} from "@react-three/fiber";
 import './Counter.css';
 
 export default function Counter({ children, count: initialCount }) {
@@ -14,6 +15,17 @@ export default function Counter({ children, count: initialCount }) {
 				<button onClick={add}>+</button>
 			</div>
 			<div className="counter-message">{children}</div>
+
+			<div className="canvas">
+			<Canvas>
+        <mesh>
+          <boxBufferGeometry />
+          <meshPhongMaterial />
+        </mesh>
+        <ambientLight args={[0xff0000]} intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} intensity={0.5} />
+      </Canvas>
+			</div>
 		</>
 	);
 }
